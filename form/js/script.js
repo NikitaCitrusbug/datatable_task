d2c5262ba80385695230cd62738a78c96c3f8e58
Nikita <nikita1.citrusbug@gmail.com>
2022-07-01 15:43:23 +0530

    completed

---
+++ form/js/script.js
@@ -0,0 +1,48 @@
+
+// btn.addEventListener('click' , function(){
+//     alert('hello......................................')
+// })
+
+function greenValidation(){
+    // var green = document.getElementById('green');
+    // console.log(blur);
+    // green.classList.greenValidation('active');
+    window.location.href = "registration.html";
+
+    // alert('green box selected.........................')
+
+}
+
+function blueValidation(){
+    window.location.href = "registration1.html";
+    // alert("blue box selected..............................")
+}
+
+// function toggle(){
+//     var blur = document.getElementById('blur');
+//     blur.classList.toggle('active');
+//     // alert("hover.............................................")
+// }
+
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    console.log(blur);
+    blur.classList.toggle('active');
+    var visible = document.getElementById('hidden');
+    visible.classList.toggle('active');
+    // alert("helllooooooooooooooooooooooooooooooooooooooooooo")
+}
+// function toggleHidden(){
+//     var visible = document.getElementById('hidden');
+//     visible.classList.toggle('active');
+//     alert("helllooooooooooooooooooooooooooooooooooooooooooo")
+// }
+
+function submitForm(){
+    alert("Thnak you for the registration...............")
+}
+
+function submitForm1(){
+    alert("Thnak you for the registration...............")
+}
